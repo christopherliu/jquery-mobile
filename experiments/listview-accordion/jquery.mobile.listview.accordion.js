dfe91c474de79619dc90a8293eb96f692ccbab3f
Christopher Liu (of course!) <github@christopherliu.net>
2011-02-22 09:40:52 -0800

    Class ui-li-accordion-open can now be specified in advance to keep a panel initially open.

--- experiments/listview-accordion/jquery.mobile.listview.accordion.js
+++ experiments/listview-accordion/jquery.mobile.listview.accordion.js
@@ -1,6 +1,6 @@
 /*
 * jQuery Mobile Framework : listview accordion extension
-* Copyright (c) Boris Smus
+* Copyright (c) Boris Smus, Christopher Liu
 * Note: Code is in draft form and is subject to change 
 */
 (function($, undefined ) {
@@ -8,7 +8,13 @@
 $( "[data-role='listview']" ).live( "listviewcreate", function() {
 	var list = $( this ),
 		listview = list.data( "listview" );
-	
+
+  var accordionExpandOne = function(accordion) {
+	// Close all other accordion flaps
+	list.find('.ui-li-accordion').slideUp();
+	// Open this flap 
+	accordion.slideToggle();
+  }
   var accordionDecorator = function() {
   	list.find('.ui-li-accordion').each(function(index, accordion) {
   		// Format the accordion accordingly:
@@ -29,23 +35,27 @@ $( "[data-role='listview']" ).live( "listviewcreate", function() {
   		$li.find('a').remove();
   		// Bind click handler to show the accordion
   		$li.bind('click', function() {
-  			// Check that the current flap isn't already open
   			var $accordion = $(this).find('.ui-li-accordion');
-  			if ($accordion.css('display') != 'none') {
+  			// Check that the current flap isn't already open
+  			if ($accordion.hasClass('ui-li-accordion-open')) {
   				$accordion.slideUp();
   				$(this).removeClass('ui-li-accordion-open');
   				return;
   			}
-  			// Close all other accordion flaps
-  			list.find('.ui-li-accordion').slideUp();
-  			// Open this flap 
-  			$accordion.slideToggle();
+  			// If not, clear old classes
+  			list.find('.ui-li-accordion-open').removeClass('ui-li-accordion-open');
   			$(this).toggleClass('ui-li-accordion-open');
+  			accordionExpandOne($accordion);
   		});
   	});
 	};
+	var accordionExpandInitial = function() {
+		//Expand anything already marked with -open.
+		accordionExpandOne(list.find('.ui-li-accordion-open'));
+	};
 	
 	accordionDecorator();
+	accordionExpandInitial();
 	
 	// Make sure that the decorator gets called on listview refresh too
   var orig = listview.refresh;
